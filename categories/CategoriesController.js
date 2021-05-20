eecb819185fd2cd1070193581e28766bd7913512
gustavow1985 <51914845+gustavow1985@users.noreply.github.com>
2021-05-20 08:04:33 -0300

    cria navbar

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-    res.send("Categories router")
-});
-
 router.get("/admin/categories/new", (req, res) => {
-    res.send("New categories router")
+    res.render("admin/categories/new");
 });
 
 module.exports = router;
