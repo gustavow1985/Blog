576bfe2474954cf9a1da69a1321c622766f95c0f
gustavow1985 <51914845+gustavow1985@users.noreply.github.com>
2021-05-24 06:51:33 -0300

    cria view para exibição dos artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -4,22 +4,29 @@ const Category = require("../categories/Category");
 const Article = require("./Article");
 const slugify = require("slugify");
 
+router.get("/admin/articles", (req, res) => {
+    res.render("admin/articles/index")
+});
+
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", { categories })
-    })
+    });
 });
 
 router.post("/articles/save", (req, res) => {
     var title = req.body.title;
     var body = req.body.body;
-    var id = req.body.id;
+    var category = req.body.category;
 
-    Article. create({
-        title,
+
+    Article.create({
+        title: title,
         slug: slugify(title),
         body,
-        categoryId //foreign KEY
+        categoryId: category //foreign KEY
+    }).then(() => {
+        res.redirect("/admin/articles")
     })
 });
 
--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const Sequeliza = require('sequelize');
 const connection = new Sequeliza('blog', 'root', 'Asa123123', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone:"-03:00" //ajusta timezone
 });
 
 module.exports = connection;
