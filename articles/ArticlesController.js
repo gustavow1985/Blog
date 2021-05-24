bc1d62f8f8552b091d92f67569b01068bd308eb9
gustavow1985 <51914845+gustavow1985@users.noreply.github.com>
2021-05-24 06:24:38 -0300

    cia rota para a criação de artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const Category = require("../categories/Category")
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", { categories })
     })
-})
-   
+});
 
-// router.get("/admin/articles", (req, res) => {
-//     res.send("Articles router")
-// });
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var id = req.body.id;
+
+    Article. create({
+        title,
+        slug: slugify(title),
+        body,
+        categoryId //foreign KEY
+    })
+});
 
 module.exports = router;
