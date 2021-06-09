7b5ebda4cd55cfb3492962eba66450c5c84bc7ad
gustavow1985 <51914845+gustavow1985@users.noreply.github.com>
2021-06-09 06:45:01 -0300

    seleciona categoria de acordo com o artigo que será editado

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -56,7 +56,7 @@ router.get("/admin/articles/edit/:id", (req, res) =>{
     Article.findByPk(id).then(article =>{
         if(article != undefined){
             Category.findAll().then(categories =>{
-                res.render("admin/articles/edit", { categories })
+                res.render("admin/articles/edit", { categories, article })
             })
         } else {
             res.redirect("/")
