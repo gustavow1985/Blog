d61e9997822e9df5539c755face9273a809e3d92
gustavow1985 <51914845+gustavow1985@users.noreply.github.com>
2021-06-15 07:06:08 -0300

    cria model de usuario

---
+++ user/UserController.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+
+const User = connection.define('users', {
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }, password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+// Category.sync({force: true}); //cria tabela novamente sempre que o programa for executado
+
+module.exports = User;
