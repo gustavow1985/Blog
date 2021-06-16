5faac7f9fa6cd86e2f99f5f375f4c6aedcbde68f
gustavow1985 <51914845+gustavow1985@users.noreply.github.com>
2021-06-16 07:03:20 -0300

    verifica possivel duplicidade de cadastro

--- users/UsersController.js
+++ users/UsersController.js
@@ -15,18 +15,25 @@ router.post("/users/create", (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(password, salt);
-
-    User.create ({
-        email,
-        password: hash
-    }).then(() => {
-        res.redirect("/");
-    }).catch((err) => {
-        res.redirect("/");
+    User.findOne({ where: {email }}).then (user => {
+        if (user == undefined){
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+        
+            User.create ({
+                email,
+                password: hash
+            }).then(() => {
+                res.redirect("/");
+            }).catch((err) => {
+                res.redirect("/");
+            });
+        }else{
+            res.redirect("/admin/users/create");
+        }
     })
-})
+
+});
 
 
 module.exports = router;
