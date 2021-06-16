e7cbf1801ba22a3e5b8f904bb15a3210cf66da66
gustavow1985 <51914845+gustavow1985@users.noreply.github.com>
2021-06-16 07:31:32 -0300

    cria tela de usuários cadastrados

--- users/UsersController.js
+++ users/UsersController.js
@@ -4,7 +4,9 @@ const User = require("./User");
 const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req, res) => {
-    res.send("Lista de usuários");
+    User.findAll().then(users => {
+        res.render("admin/users/index", {users});
+    });
 });
 
 router.get("/admin/users/create", (req, res) => {
